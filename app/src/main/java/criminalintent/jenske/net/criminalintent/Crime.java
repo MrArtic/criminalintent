c3f356bdccdd7c26b3bf79b6843b802352e75c66
MrArtic <jenskristian@gmail.com>
2014-08-21 20:58:42 +0200

    Update with identifier

--- app/src/main/java/criminalintent/jenske/net/criminalintent/Crime.java
+++ app/src/main/java/criminalintent/jenske/net/criminalintent/Crime.java
@@ -1,7 +1,34 @@
 package criminalintent.jenske.net.criminalintent;
 
+import java.util.UUID;
+
 /**
- * Created by Artic on 21/08/14.
+ * Created by Jens Kristian Espevik on 21/08/14.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+
+    // Getter and setters
+
+    public UUID getId() {
+
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+
 }
