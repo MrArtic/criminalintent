381f2515651522211c4730af3908c47c6f8bf6f9
MrArtic <jenskristian@gmail.com>
2014-08-23 15:08:41 +0200

    delete auto-generated menu

--- app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeActivity.java
+++ app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeActivity.java
@@ -2,7 +2,6 @@ package criminalintent.jenske.net.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.MenuItem;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -16,15 +15,5 @@ public class CrimeActivity extends FragmentActivity {
 
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 }
