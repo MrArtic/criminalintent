1e5a69df51b44dfacadf1599af929d2e8f570146
MrArtic <jenskristian@gmail.com>
2014-08-23 15:42:29 +0200

    inflate fragment

--- app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeFragment.java
+++ app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package criminalintent.jenske.net.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Jens Kristian Espevik on 23/08/14.
@@ -14,5 +17,11 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        return v;
     }
 }
