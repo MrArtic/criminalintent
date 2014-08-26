1954f7239d9f737038ab1be0a4feeb5cd4d3df4d
MrArtic <jenskristian@gmail.com>
2014-08-26 20:01:05 +0200

    add ArrayList for holding Crimes

--- app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeLab.java
+++ app/src/main/java/criminalintent/jenske/net/criminalintent/CrimeLab.java
@@ -2,16 +2,21 @@ package criminalintent.jenske.net.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  * Created by Jens Kristian Espevik on 26/08/14.
  */
 public class CrimeLab {
+    private ArrayList<Crime> mCrimes;
 
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
 
     private CrimeLab(Context appContext){
         mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
     }
 
     public static CrimeLab get(Context c){
@@ -19,5 +24,17 @@ public class CrimeLab {
             sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
         return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for (Crime c : mCrimes){
+            if(c.getId().equals(id))
+                return c;
+        }
+        return null;
     }
 }
